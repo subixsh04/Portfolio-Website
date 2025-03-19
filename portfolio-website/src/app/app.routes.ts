a574eaa391aab1f8a718745ffe9c244ed5e4e98a
subixsh04 <subikshasvaidhyanathan@gmail.com>
2025-03-19 17:23:20 -0400

    added bio and profile image for home

--- portfolio-website/src/app/app.routes.ts
+++ portfolio-website/src/app/app.routes.ts
@@ -1,3 +1,10 @@
-import { Routes } from '@angular/router';
+// app.routes.ts
 
-export const routes: Routes = [];
+import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'projects', loadComponent: () => import('./components/projects/projects.component').then(m => m.ProjectsComponent) },
+  { path: '**', redirectTo: '' } // Redirect unknown paths to Home
+];
